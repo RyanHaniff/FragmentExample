69e2ee3e64a931e5a04928bd092cc9a53245fcf0
RyanH <haniffryan.hr@gmail.com>
2016-11-15 17:11:00 -0500

    Added some comments on how the fragment get implemented in the main activity

--- app/src/main/java/com/example/android/fragmentexample/MainActivity.java
+++ app/src/main/java/com/example/android/fragmentexample/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.android.fragmentexample;
 
-import android.app.FragmentTransaction;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,17 +10,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //Fragment One
         FragmentOne firstFragment = new FragmentOne();
 
         firstFragment.setArguments(getIntent().getExtras());
 
+        //for the getSupportFragmentManager to work the fragment object that your are repalcing the
+        //frame layou with must import the: import android.support.v4.app.Fragment;
+        //if not you will get an error saying it cannot resolve the method add()
         getSupportFragmentManager().beginTransaction().add(R.id.fragment_one, firstFragment).commit();
 
+        //Fragment Two
         FragmentTwo secondFragment = new FragmentTwo();
 
+        // In case this activity was started with special instructions from an
+        // Intent, pass the Intent's extras to the fragment as arguments
         secondFragment.setArguments(getIntent().getExtras());
 
+        // Add the fragment to the 'fragment_two' FrameLayout
         getSupportFragmentManager().beginTransaction().add(R.id.fragment_two, secondFragment).commit();
 
     }
