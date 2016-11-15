c12be6091a654be1bbc96e88a71b3c65b01eeefe
RyanH <haniffryan.hr@gmail.com>
2016-11-15 17:06:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/fragmentexample/FragmentTwo.java
@@ -0,0 +1,21 @@
+package com.example.android.fragmentexample;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Ryan PC on 2016-11-15.
+ */
+
+public class FragmentTwo extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+}
